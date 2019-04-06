054c866741b38a150ad6136db874669a4d35a03d
DerpyCrabs <derpycrabs@gmail.com>
2019-04-07 00:18:30 +0700

    Initial implementation of single notes

--- src/main.rs
+++ src/main.rs
@@ -4,80 +4,105 @@ use std::io::prelude::*;
 extern crate quicksilver;
 
 use quicksilver::{
+    geom::{Rectangle, Shape, Transform, Vector},
+    graphics::{Background::Col, Background::Img, Color, Font, FontStyle, Image, View},
+    input::Key,
+    lifecycle::{run, Asset, Settings, State, Window},
     Result,
-    geom::{Line, Rectangle, Shape, Transform, Triangle, Vector},
-    graphics::{Background::Col, Color, View},
-    input::{Key},
-    lifecycle::{Settings, State, Window, run},
 };
 
-use osu_format::{Beatmap, HitObjectBase, HitObject};
+use osu_format::{Beatmap, HitObject};
 
 type MapObj = [bool; 7];
 
 struct Camera {
-    view: Rectangle,
     beatmap: Beatmap,
-    speed: i32,
+    speed: f64,
     duration: u32,
     map: Vec<MapObj>,
-    current: i32,
+    position: f64,
+    score: i64,
+    play300: f64,
+    asset: Asset<Image>,
+    buttons: [bool; 7],
 }
 
 fn x_to_note(x: u32) -> MapObj {
-  match x {
-    36 => [true, false, false, false, false, false, false],
-    109 =>[false, true, false, false, false, false, false],
-    182 =>[false, false, true, false, false, false, false],
-    256 =>[false, false, false, true, false, false, false],
-    329 =>[false, false, false, false, true, false, false],
-    402 =>[false, false, false, false, false, true, false],
-    475 =>[false, false, false, false, false, false, true],
-    _ => [false, false, false, false, false, false, false]
-  }
+    match x {
+        36 => [true, false, false, false, false, false, false],
+        109 => [false, true, false, false, false, false, false],
+        182 => [false, false, true, false, false, false, false],
+        256 => [false, false, false, true, false, false, false],
+        329 => [false, false, false, false, true, false, false],
+        402 => [false, false, false, false, false, true, false],
+        475 => [false, false, false, false, false, false, true],
+        _ => [false, false, false, false, false, false, false],
+    }
 }
 fn new_map(beatmap: &Beatmap) -> Vec<MapObj> {
     let duration = match beatmap.hit_objects.last().unwrap() {
-      HitObject::Circle{base} => base.time,
-      _ => 0,
+        HitObject::Circle { base } => base.time,
+        _ => 0,
     } + 100;
-    
+
     let mut map = Vec::new();
-    for tick in (0..duration) {
-      map.push([false, false, false, false, false, false, false]);
+    for _ in 0..duration {
+        map.push([false, false, false, false, false, false, false]);
     }
-    
+
     for obj in beatmap.hit_objects.iter() {
-      match obj {
-        HitObject::Circle{base} => map[base.time as usize] = x_to_note(base.x),
-        HitObject::LongNote{base, end_time} => {for i in (base.time..*end_time) {
-          map[i as usize] = x_to_note(base.x);
+        match obj {
+            HitObject::Circle { base } => map[base.time as usize] = x_to_note(base.x),
+            HitObject::LongNote { base, end_time } => {
+                for i in base.time..*end_time {
+                    map[i as usize] = x_to_note(base.x);
+                }
+            }
+            _ => (),
         }
-        },
-        _ => ()
-      }
     }
-    
+
     return map;
+}
+
+fn handle_keydown(map: &mut Vec<MapObj>, position: f64, index: usize) -> i64 {
+    let mut found = false;
+    for obj in (position as i32 - 400)..(position as i32 + 400) {
+        if map[obj as usize][index] == true {
+            found = true;
+            map[obj as usize][index] = false;
+            break;
+        }
+    }
+    if found {
+        return 300;
+    }
+    return 0;
 }
 
 impl State for Camera {
     fn new() -> Result<Camera> {
-    let f = std::fs::File::open("map2.osu").unwrap();
-    let f = std::io::BufReader::new(f);
-    let beatmap = osu_format::Parser::new(f.lines()).parse().unwrap();
-    let map = new_map(&beatmap);
-    let duration = match beatmap.hit_objects.last().unwrap() {
-      HitObject::Circle{base} => base.time,
-      _ => 0,
-    } + 100;
-    
+        let f = std::fs::File::open("alice.osu").unwrap();
+        let f = std::io::BufReader::new(f);
+        let beatmap = osu_format::Parser::new(f.lines()).parse().unwrap();
+        let map = new_map(&beatmap);
+        let duration = match beatmap.hit_objects.last().unwrap() {
+            HitObject::Circle { base } => base.time,
+            _ => 0,
+        } + 100;
+
+        let asset = Asset::new(Image::load("hit300.png"));
+
         Ok(Camera {
-            view: Rectangle::new_sized((800, 600)),
-    beatmap,
-    speed: 10,
-    duration, map,
-    current: 0
+            beatmap,
+            speed: 0.2,
+            duration,
+            map,
+            position: 0.0,
+            score: 0,
+            asset,
+            play300: 0.0,
+            buttons: [false, false, false, false, false, false, false],
         })
     }
 
@@ -88,33 +113,150 @@ impl State for Camera {
         // if window.keyboard()[Key::Right].is_down() {
         //     self.view = self.view.translate((4, 0));
         // }
-        if window.keyboard()[Key::Down].is_down() {
-          self.current += self.speed;
-          self.view = self.view.translate((0, 4));
+        if window.current_fps() != 0.0 {
+            self.position += 1000.0 / window.current_fps();
+            if self.play300 > 0.0 {
+                self.play300 -= 1000.0 / window.current_fps();
+            }
         }
-        if window.keyboard()[Key::Up].is_down() {
-          self.current -= self.speed;
-          self.view = self.view.translate((0, -4));
+        if window.keyboard()[Key::S].is_down() {
+            if !self.buttons[0] {
+                let score = handle_keydown(&mut self.map, self.position, 0);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[0] = true;
+            }
+        } else {
+            self.buttons[0] = false;
         }
-        window.set_view(View::new(self.view));
+        if window.keyboard()[Key::D].is_down() {
+            if !self.buttons[1] {
+                let score = handle_keydown(&mut self.map, self.position, 1);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[1] = true;
+            }
+        } else {
+            self.buttons[1] = false;
+        }
+        if window.keyboard()[Key::F].is_down() {
+            if !self.buttons[2] {
+                let score = handle_keydown(&mut self.map, self.position, 2);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[2] = true;
+            }
+        } else {
+            self.buttons[2] = false;
+        }
+        if window.keyboard()[Key::Space].is_down() {
+            if !self.buttons[3] {
+                let score = handle_keydown(&mut self.map, self.position, 3);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[3] = true;
+            }
+        } else {
+            self.buttons[3] = false;
+        }
+        if window.keyboard()[Key::J].is_down() {
+            if !self.buttons[4] {
+                let score = handle_keydown(&mut self.map, self.position, 4);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[4] = true;
+            }
+        } else {
+            self.buttons[4] = false;
+        }
+        if window.keyboard()[Key::K].is_down() {
+            if !self.buttons[5] {
+                let score = handle_keydown(&mut self.map, self.position, 5);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[5] = true;
+            }
+        } else {
+            self.buttons[5] = false;
+        }
+        if window.keyboard()[Key::L].is_down() {
+            if !self.buttons[6] {
+                let score = handle_keydown(&mut self.map, self.position, 6);
+                self.score += score;
+                if score == 300 {
+                    self.play300 = 300.0;
+                }
+                self.buttons[6] = true;
+            }
+        } else {
+            self.buttons[6] = false;
+        }
         Ok(())
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
-      let wind = 600 / self.speed;
         window.clear(Color::WHITE)?;
-        for obj in (self.current..(self.current + wind)) {
-          for (i, hit) in self.map[obj as usize].iter().enumerate() {
-            if (*hit) {
-            window.draw_ex(&Rectangle::new(((i as i32) * 73, 600/ ((obj as i32) / self.speed)), (73, 600/ wind)), Col(Color::BLUE), Transform::scale((1, -1)), 1);
+        let mut cur = 0;
+        let mut cur_tp = &self.beatmap.timing_points[cur];
+        for obj in 0..(1000.0 / self.speed) as usize {
+            for (i, hit) in self.map[self.position as usize + obj as usize]
+                .iter()
+                .enumerate()
+            {
+                if *hit {
+                    window.draw_ex(
+                        &Rectangle::new(
+                            (
+                                (i as i32) * 73,
+                                384 - (obj as f64 * (1000.0 / 384.0) * self.speed) as i32,
+                            ),
+                            (73, self.speed as f32 * cur_tp.milliseconds_per_beat / 8.0),
+                        ),
+                        Col(Color::BLUE),
+                        Transform::scale((1, -1)),
+                        1,
+                    );
+                }
             }
-          }
+        }
+        let play300 = self.play300;
+        if self.play300 > 0.0 {
+            self.asset.execute(|image| {
+                window.draw_ex(
+                    &image.area().with_center((256, 192)),
+                    Img(&image),
+                    Transform::scale((
+                        1.0 - (((150.0 - play300).abs() as f32) / 150.0),
+                        1.0 - (((150.0 - play300).abs() as f32) / 150.0),
+                    )),
+                    1,
+                );
+                Ok(())
+            });
         }
         Ok(())
     }
 }
 
-
 fn main() {
-    run::<Camera>("Camera", Vector::new(800, 600), Settings::default());
+    run::<Camera>(
+        "Camera",
+        Vector::new(512, 384),
+        Settings {
+            vsync: false,
+            ..Default::default()
+        },
+    );
 }
